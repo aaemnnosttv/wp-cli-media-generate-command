27e4ed950b0f7bd9037d148d0ba710a45f22b7b9
Evan Mattson <me@aaemnnost.tv>
2017-06-29 12:02:15 +0300

    update usage for consistency with post generate
    - added progress and ids output formats

--- features/generate-image.feature
+++ features/generate-image.feature
@@ -1,32 +1,39 @@
 Feature: Images can be generated according to given criteria and added to the media library.
 
-  Scenario: The number of images to generate must be specified.
+  Scenario: Generates 100 images by default.
     Given a WP install
-    When I try `wp media generate image`
+    When I run `wp media generate image`
     Then STDOUT should contain:
       """
-      usage: wp media generate image <count>
+      Generated 100 images
       """
-    And the media library should contain 0 items
+
+  Scenario: The number of images generated can be specified with a flag.
+    Given a WP install
+    When I run `wp media generate image --count=9`
+    Then STDOUT should contain:
+      """
+      Generated 9 images
+      """
 
   Scenario: Generated images are added to the media library.
     Given a WP install
     And the media library should contain 0 items
 
-    When I run `wp media generate image 10`
+    When I run `wp media generate image --count=10`
     Then the media library should contain 10 items
 
   Scenario: The default generated size for each image is small enough to only create a single file per item.
     Given a WP install
-    When I run `wp media generate image 1`
+    When I run `wp media generate image --count=1`
     And the uploads directory should contain 1 file
 
   Scenario: Images can be generated with specific dimensions.
     Given a WP install
-    When I run `wp media generate image 1 123x45`
+    When I run `wp media generate image --count=1 --size=123x45`
     Then the uploads directory should contain 1 image 123px wide and 45px tall
 
   Scenario: Images can be generated with a given file extension.
     Given a WP install
-    When I run `wp media generate image 1 --ext=gif`
+    When I run `wp media generate image --count=1 --ext=gif`
     Then the uploads directory should contain 1 gif
--- src/MediaGenerateCommand.php
+++ src/MediaGenerateCommand.php
@@ -22,12 +22,25 @@ class MediaGenerateCommand
 	protected $tempDir;
 
 	/**
+	 * @var \cli\progress\Bar
+	 */
+	protected $progress;
+
+	/**
+	 * @var array [singular, plural]
+	 */
+	protected $type;
+
+	/**
 	 * Generate images.
 	 *
-	 * <count>
+	 * [--count=<number>]
 	 * : Number of images to generate.
+	 * ---
+	 * default: 100
+	 * ---
 	 *
-	 * [<dimensions>]
+	 * [--size=<size>]
 	 * : Image dimensions in LxW format.
 	 * ---
 	 * default: 50x50
@@ -39,23 +52,88 @@ class MediaGenerateCommand
 	 * default: jpg
 	 * ---
 	 *
+	 * [--format=<format>]
+	 * : Render output in a particular format.
+	 * ---
+	 * default: progress
+	 * options:
+	 *   - progress
+	 *   - ids
+	 * ---
+	 *
 	 * @param $_
 	 * @param $assoc
 	 */
 	public function image($_, $assoc)
 	{
-		list($count, $dimensions) = $_;
-		list($width, $height) = explode('x', $dimensions);
 		$this->assoc = $assoc;
 		$this->tempDir = $this->generateTempDirectory();
+		$this->type = ['image', 'images'];
+		$this->initProgress();
 
+		list($width, $height) = explode('x', $assoc['size']);
 		$batch = substr(uniqid(), -5);
 		$args = compact('batch','width','height');
 
-		foreach (range(1, $count) as $i) {
-			$this->generateImage($i, $args);
+		$results = [];
+		foreach (range(1, $assoc['count']) as $i) {
+			 $results[] = $this->generateImage($i, $args);
+		}
+
+		$generated = array_filter($results);
+
+		$this->formatOutput($generated);
+	}
+
+	protected function formatOutput($generated)
+	{
+		$this->progress->finish();
+
+		$generated_count = count($generated);
+
+		if ('ids' == $this->assoc['format']) {
+			echo join(' ', $generated);
+			return;
+		}
+
+		if ($generated_count < $this->assoc['count']) {
+			WP_CLI::warning('Some items were not generated.');
+		}
+
+		WP_CLI::success(vsprintf("Generated %s %s.", [
+			$generated_count,
+			$this->typeForm($generated_count),
+		]));
+	}
+
+	protected function typeForm($count)
+	{
+		list($singular, $plural) = $this->type;
+
+		return _n($singular, $plural, $count);
+	}
+
+	/**
+	 * Initialize the progress bar for display.
+	 *
+	 * If progress is not the designated output format, a null progress bar is used instead.
+	 *
+	 * @param string $message
+	 */
+	protected function initProgress($message = '')
+	{
+		if (! $message) {
+			$message = 'Generating ' . $this->typeForm(2);
+		}
+
+		if ('progress' == $this->assoc['format']) {
+			$this->progress = \WP_CLI\Utils\make_progress_bar($message, $this->assoc['count']);
+			$this->progress->display();
+		} else {
+			$this->progress = new WP_CLI\NoOp();
 		}
 	}
+
 
 	/**
 	 * Generate an individual image.
@@ -82,6 +160,8 @@ class MediaGenerateCommand
 			'tmp_name' => $filepath,
 			'name' => Utils\basename($filepath),
 		], 0);
+
+		$this->progress->tick();
 
 		if (is_wp_error($id)) {
 			return false;
