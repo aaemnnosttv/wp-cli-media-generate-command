19d2dba6360466ec03793d860107b4be0935a867
Evan Mattson <me@aaemnnost.tv>
2017-07-19 22:32:28 +0300

    only require the autoloader if it exists

--- command.php
+++ command.php
@@ -6,7 +6,9 @@ if (! class_exists('WP_CLI')) {
 	return;
 }
 
-require_once(__DIR__ . '/vendor/autoload.php');
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+	require_once(__DIR__ . '/vendor/autoload.php');
+}
 
 WP_CLI::add_command('media generate', MediaGenerateCommand::class);
 
